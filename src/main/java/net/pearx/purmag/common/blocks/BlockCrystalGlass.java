231c5035704a0cb6e26fa59d28a021f074682fb6
mrAppleXZ <mr.applexz@gmail.com>
2017-05-14 21:07:12 +0500

    Fixed glass

--- src/main/java/net/pearx/purmag/common/blocks/BlockCrystalGlass.java
+++ src/main/java/net/pearx/purmag/common/blocks/BlockCrystalGlass.java
@@ -1,8 +1,14 @@
 package net.pearx.purmag.common.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.pearx.purmag.common.Utils;
 
 /**
@@ -28,5 +34,21 @@ public class BlockCrystalGlass extends BlockSingleSip
     public boolean isOpaqueCube(IBlockState state)
     {
         return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    {
+        Block off = blockAccess.getBlockState(pos.offset(side)).getBlock();
+        if(off == this)
+            return false;
+        return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
     }
 }
