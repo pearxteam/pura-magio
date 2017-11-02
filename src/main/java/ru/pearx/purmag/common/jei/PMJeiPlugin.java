d93c1e1b3d5fbc0935030537bffc69e754367527
mrAppleXZ <mr.applexz@gmail.com>
2017-11-02 21:05:57 +0500

    Added the Transfer Handler

--- src/main/java/ru/pearx/purmag/common/jei/PMJeiPlugin.java
+++ src/main/java/ru/pearx/purmag/common/jei/PMJeiPlugin.java
@@ -3,8 +3,15 @@ package ru.pearx.purmag.common.jei;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
+import mezz.jei.api.recipe.transfer.IRecipeTransferInfo;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -23,6 +30,7 @@ import ru.pearx.purmag.common.recipes.recipes.AbstractMagibenchRecipe;
 import ru.pearx.purmag.common.recipes.recipes.MagibenchRecipe;
 import ru.pearx.purmag.common.recipes.recipes.MagibenchShapelessRecipe;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,7 +58,39 @@ public class PMJeiPlugin implements IModPlugin
                 lst.add(new MagibenchShapelessRecipeWrapper((MagibenchShapelessRecipe)rec, registry.getJeiHelpers().getStackHelper()));
         }
         registry.addRecipes(lst, MagibenchRecipeCategory.ID);
-        registry.addRecipeClickArea(GuiContainerMagibench.class, );
+        registry.getRecipeTransferRegistry().addRecipeTransferHandler(new IRecipeTransferInfo<ContainerMagibench>()
+        {
+            @Override
+            public Class<ContainerMagibench> getContainerClass()
+            {
+                return ContainerMagibench.class;
+            }
+
+            @Override
+            public String getRecipeCategoryUid()
+            {
+                return MagibenchRecipeCategory.ID;
+            }
+
+            @Override
+            public boolean canHandle(ContainerMagibench container)
+            {
+                return true;
+            }
+
+            @Override
+            public List<Slot> getRecipeSlots(ContainerMagibench container)
+            {
+                return container.inventorySlots.subList(1, container.tier.getWidth() * container.tier.getHeight() + 1);
+            }
+
+            @Override
+            public List<Slot> getInventorySlots(ContainerMagibench container)
+            {
+                int fr = container.tier.getWidth() * container.tier.getHeight() + 2;
+                return container.inventorySlots.subList(fr, container.inventorySlots.size() - 1);
+            }
+        });
     }
 
     @Override
