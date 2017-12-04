d8b7258f5f34cc439f1bb2c3726be2700037dee8
mrAppleXZ <mr.applexz@gmail.com>
2017-12-04 16:10:20 +0500

    IF work

--- src/main/java/ru/pearx/purmag/client/gui/microscope/GuiMicroscopeResearch.java
+++ src/main/java/ru/pearx/purmag/client/gui/microscope/GuiMicroscopeResearch.java
@@ -257,7 +257,8 @@ public class GuiMicroscopeResearch extends GuiAbstractMicroscope
                     }
                 }
             }
-            sb.deleteCharAt(sb.length() - 1);
+            if(sb.length() > 0)
+                sb.deleteCharAt(sb.length() - 1);
             return sb.toString();
         }
     }
