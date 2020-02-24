9b636fcf306ea841c96ab4bb761340b1bc04a2d8
mrAppleXZ <mr.applexz@gmail.com>
2020-02-24 23:47:08 +0500

    Initial code

---
+++ src/main/kotlin/net/pearx/puramagio/Reference.kt
@@ -0,0 +1,9 @@
+package net.pearx.puramagio
+
+const val ID = "pura-magio"
+const val NAME = "Pura Magio"
+const val DESCRIPTION = ""
+const val VERSION = ""
+const val ACCEPTED_MINECRAFT_VERSIONS = ""
+const val DEPENDENCIES = ""
+val AUTHORS = listOf("mrAppleXZ", "gt22", "KoldMag")
